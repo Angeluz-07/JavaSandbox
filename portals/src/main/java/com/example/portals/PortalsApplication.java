04162d85dd6f312110cbe724072b158eaee856aa
Angeluz-07 <alexandro.ramr777@gmail.com>
2024-02-06 16:42:29 -0500

    add portals app

---
+++ portals/src/main/java/com/example/portals/PortalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.portals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalsApplication.class, args);
+	}
+
+}
